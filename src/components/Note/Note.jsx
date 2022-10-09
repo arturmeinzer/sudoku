cbb04e25f7b4b7b4385343c4b1ffe3c2d95a1b6d
Artur Meinzer <artur.meinzer@web.de>
2022-10-09 15:14:33 +0200

    Fix note block for mobile breakpoints

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -3,14 +3,22 @@ import PropTypes from "prop-types";
 
 const Note = styled.div`
     display: inline-block;
-    width: 14px;
-    height: 14px;
-    line-height: 14px;
-    font-size: 12px;
+    width: 10px;
+    height: 10px;
+    line-height: 9px;
+    font-size: 9px;
     text-align: center;
     vertical-align: middle;
     color: #555;
-    margin: 2px;
+    margin: 1px;
+
+    @media(min-width: 560px) {
+        width: 14px;
+        height: 14px;
+        line-height: 14px;
+        font-size: 12px;
+        margin: 2px;
+    }
   
     ${(props) => props.highlight && css`
         background: cornflowerblue;
