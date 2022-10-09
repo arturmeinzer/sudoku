e8658006c57873a6cef5227c732d1e923c4a5057
Artur Meinzer <artur.meinzer@web.de>
2022-10-09 10:50:44 +0200

    Fix mobile view

--- src/components/ButtonGroup/ButtonGroup.jsx
+++ src/components/ButtonGroup/ButtonGroup.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 const ButtonGroup = styled(Box)`
     display: flex;
     justify-content: center;
+    flex-wrap: wrap;
     align-items: center;
     padding: 10px;
     gap: 20px;
--- src/components/Puzzle/Puzzle.jsx
+++ src/components/Puzzle/Puzzle.jsx
@@ -2,12 +2,16 @@ import styled from "styled-components";
 
 const Puzzle = styled.div`
     display: grid;
-    grid-template-columns: repeat(9, 60px);
-    grid-template-rows: repeat(9, 60px);
+    width: 360px;
+    grid-template-columns: repeat(9, 40px);
+    grid-template-rows: repeat(9, 40px);
     border: 2px solid #000;
-    margin: 10px 0;
-    width: 540px;
-    height: 540px;
+    margin: 10px auto;
+    @media (min-width: 560px) {
+        width: 540px;
+        grid-template-columns: repeat(9, 60px);
+        grid-template-rows: repeat(9, 60px);
+    }
 `;
 
 export default Puzzle;
